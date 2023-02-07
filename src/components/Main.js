208978f11ceabccb5b8339d6f317f0fefc0b1204
Ivan Sekulic <ivansekulic4@gmail.com>
2023-02-07 13:05:36 +0100

    main section added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default function Main(){
-    return <h1>Main component</h1>
+    return(
+        <main>
+            <h1 className="main--title">React facts</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was created by Jordan walke</li>
+                <li>It maintained by Facebook</li>
+            </ul>
+        </main>
+    )
 }
